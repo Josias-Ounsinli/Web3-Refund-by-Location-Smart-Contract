1e3b06b70fd07fcc8c025676c08de0ca098555d0
Josias-Ounsinli <josias.ouns.djossou@gmail.com>
2022-10-29 12:32:33 +0100

    Exploring Smart-Contract implementation with hardhat

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  // networks: {
+  //   goerli: {
+  //     url: 'https://eth-goerli.g.alchemy.com/v2/bExZ7vJOlLI9M6TtSx2M3JgnYnOvqE4j',
+  //     accounts: '0x0cc57277B8D6da5536c39eaf78532Abd62fc42eB'
+  //   }
+  // },
 };
